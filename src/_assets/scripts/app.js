275e76c50ab559d4e368ca71fc25b72168364a95
julianiff <julian.iff@helsana.ch>
2021-09-22 14:27:00 +0200

    f remove unused css and remove fast skip button

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -1,1 +1,3 @@
+/** Import Webcomponents Loader to have it present in html Templates */
+import "@webcomponents/webcomponentsjs/webcomponents-loader.js";
 console.log(`I was loaded at ${Date(Date.now()).toString()}`);
